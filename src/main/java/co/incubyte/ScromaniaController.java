82d0b252344b20a3e4fa070d9a0e2ca330c9dfa4
Sapan Parikh <sapanparikh18@gmail.com>
2023-02-10 15:52:45 +0530

    Removes Sonar quality issues

--- src/main/java/co/incubyte/ScromaniaController.java
+++ src/main/java/co/incubyte/ScromaniaController.java
@@ -7,10 +7,6 @@ public class ScromaniaController {
 
     @Get(uri = "/", produces = "text/plain")
     public String index() {
-        String value = null;
-        if(value.equals("this")){
-            System.out.println("that");
-        }
         return "Example Response";
     }
 }
