1696f41ca9c0f35d5d10174390ce823191e0eea2
Akshay Vadher <ce.akshay@gmail.com>
2023-02-14 16:04:55 +0530

    CNL: Removed un necessary code related to app insights

--- src/main/java/co/incubyte/ScromaniaController.java
+++ src/main/java/co/incubyte/ScromaniaController.java
@@ -1,12 +1,17 @@
 package co.incubyte;
 
 import io.micronaut.http.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller("/scromania")
 public class ScromaniaController {
 
+  private static final Logger log = LoggerFactory.getLogger(ScromaniaController.class);
+
     @Get(uri = "/", produces = "text/plain")
     public String index() {
+        log.info("requesting a resource");
         return "Example Response";
     }
 }
