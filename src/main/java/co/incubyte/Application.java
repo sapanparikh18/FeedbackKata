802b3696072ed2ba39dd51930369c678db6b0906
Sapan Parikh <sapanparikh18@gmail.com>
2023-02-14 11:15:10 +0530

    Attaches app insight in a static block

--- src/main/java/co/incubyte/Application.java
+++ src/main/java/co/incubyte/Application.java
@@ -4,9 +4,10 @@ import com.microsoft.applicationinsights.attach.ApplicationInsights;
 import io.micronaut.runtime.Micronaut;
 
 public class Application {
-
-    public static void main(String[] args) {
+    static {
         ApplicationInsights.attach();
+    }
+    public static void main(String[] args) {
         Micronaut.run(Application.class, args);
     }
 }
