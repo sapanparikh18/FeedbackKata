8d23054931d8afa4fb68102154bbe6ea039cfb1c
Sapan Parikh <sapanparikh18@gmail.com>
2023-02-14 11:20:44 +0530

    Attaches app insight in an event listener

--- src/main/java/co/incubyte/Application.java
+++ src/main/java/co/incubyte/Application.java
@@ -4,9 +4,6 @@ import com.microsoft.applicationinsights.attach.ApplicationInsights;
 import io.micronaut.runtime.Micronaut;
 
 public class Application {
-    static {
-        ApplicationInsights.attach();
-    }
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
     }
