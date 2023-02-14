b8f377f93b8d1de85befe76094afa0dcbd18db3a
Sapan Parikh <sapanparikh18@gmail.com>
2023-02-14 10:30:15 +0530

    Attaches app insight through code Application.java

--- src/main/java/co/incubyte/Application.java
+++ src/main/java/co/incubyte/Application.java
@@ -1,10 +1,12 @@
 package co.incubyte;
 
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
 import io.micronaut.runtime.Micronaut;
 
 public class Application {
 
     public static void main(String[] args) {
+        ApplicationInsights.attach();
         Micronaut.run(Application.class, args);
     }
 }
