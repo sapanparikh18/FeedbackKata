8001d36fab3693026d4b7633045b562a500bae49
Akshay Vadher <ce.akshay@gmail.com>
2023-02-14 17:22:04 +0530

    ENH: Storing count to check if load test has been generated

--- src/main/java/co/incubyte/ScromaniaController.java
+++ src/main/java/co/incubyte/ScromaniaController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 public class ScromaniaController {
 
   private static final Logger log = LoggerFactory.getLogger(ScromaniaController.class);
+  private Long requestCount = 0L;
 
   private final ServerEventListener serverEventListener;
 
@@ -19,6 +20,6 @@ public class ScromaniaController {
   @Get(uri = "/", produces = "text/plain")
   public String index() {
     log.info("requesting a resource");
-    return "Example Response 0.2 at " + serverEventListener.getStartedAt();
+    return "Example Response 0.2 at " + serverEventListener.getStartedAt() + " with " + (++requestCount) + " request count";
   }
 }
