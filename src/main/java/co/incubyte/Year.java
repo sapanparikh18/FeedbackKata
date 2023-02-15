abae0411d3869972fc240687c69343031a4b9692
Sapan Parikh <sapanparikh18@gmail.com>
2023-02-15 22:54:45 +0530

    Step-3 force Sonar Error

--- src/main/java/co/incubyte/Year.java
+++ src/main/java/co/incubyte/Year.java
@@ -3,20 +3,20 @@ package co.incubyte;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Year {
-    private final int inputYear;
+    private final int year;
     private boolean leap;
 
     public void setLeap(boolean leap) {
         this.leap = leap;
     }
     public Year(@JsonProperty("year") int year) {
-        this.inputYear = year;
+        this.year = year;
     }
     public boolean isLeap() {
         return leap;
     }
 
     public int getInputYear() {
-        return inputYear;
+        return year;
     }
 }
