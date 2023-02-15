0191f5e7fa870f6beb7ce956ae3f75f2bbeb80d0
Sapan Parikh <sapanparikh18@gmail.com>
2023-02-15 22:27:17 +0530

    Step-2 start

--- src/main/java/co/incubyte/Year.java
+++ src/main/java/co/incubyte/Year.java
@@ -3,17 +3,20 @@ package co.incubyte;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Year {
-    private final int year;
+    private final int inputYear;
     private boolean leap;
 
     public void setLeap(boolean leap) {
         this.leap = leap;
     }
     public Year(@JsonProperty("year") int year) {
-        this.year = year;
+        this.inputYear = year;
     }
     public boolean isLeap() {
         return leap;
     }
 
+    public int getInputYear() {
+        return inputYear;
+    }
 }
