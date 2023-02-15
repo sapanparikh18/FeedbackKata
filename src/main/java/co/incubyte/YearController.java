db9c88865835f2e9ffd77978a706e16833d10f99
Sapan Parikh <sapanparikh18@gmail.com>
2023-02-15 22:00:17 +0530

    Step-2 start

---
+++ src/main/java/co/incubyte/YearController.java
@@ -0,0 +1,19 @@
+package co.incubyte;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+@Controller("year")
+public class YearController {
+    @Get("/{year}")
+    public Year isLeap(int year) {
+        Year responseYear = new Year(year);
+        boolean isLeap = isDivisibleBy(year, 100) ? isDivisibleBy(year, 400) : isDivisibleBy(year, 4);
+        responseYear.setLeap(isLeap);
+        return responseYear;
+    }
+
+    private static boolean isDivisibleBy(int year, int x) {
+        return year % x == 0;
+    }
+}
--- src/test/java/co/incubyte/ScromaniaTest.java
+++ src/test/java/co/incubyte/ScromaniaTest.java
@@ -1,11 +1,15 @@
 package co.incubyte;
 
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
-
 import jakarta.inject.Inject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 @MicronautTest
 class ScromaniaTest {
@@ -16,5 +20,14 @@ class ScromaniaTest {
     void testItWorks() {
         Assertions.assertTrue(application.isRunning());
     }
+    @Inject
+    @Client("/year")
+    HttpClient client;
 
+    @Test
+    @DisplayName("should return leap if year divisible by 400")
+    public void should_return_yes_if_year_divisible_by_400() {
+        Year year = client.toBlocking().retrieve("/4000", Year.class);
+        assertThat(year.isLeap()).isTrue();
+    }
 }
