ce2bd77871827d350a72c74a50a413739f9093a1
Sapan Parikh <sapanparikh18@gmail.com>
2023-02-16 12:07:38 +0530

    Step 3 inital stage

--- src/main/java/co/incubyte/YearController.java
+++ src/main/java/co/incubyte/YearController.java
@@ -8,7 +8,12 @@ public class YearController {
     @Get("/{year}")
     public Year isLeap(int year) {
         Year responseYear = new Year(year);
+        boolean isLeap = isDivisibleBy(year, 100) ? isDivisibleBy(year, 400) : isDivisibleBy(year, 4);
+        responseYear.setLeap(isLeap);
         return responseYear;
     }
 
+    private static boolean isDivisibleBy(int year, int x) {
+        return year % x == 0;
+    }
 }
